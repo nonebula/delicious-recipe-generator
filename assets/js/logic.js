172aedb0f623e684cacd6d1f5f2d35a9864dd9a8
celiahouston <celia.houston99@gmail.com>
2023-12-31 23:08:49 +0000

    html and js fully integrated correctly and arrays updated.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -111,91 +111,10 @@ if (saveButton) {
     };
     localStorage.setItem("userChoices", JSON.stringify(selections));
     window.location.href = "results.html";
-  });
-} 
-const fetchButton = document.querySelector("#js-fetch-recipes");
-const recipeResults = document.querySelector("#js-recipe-section"); 
-
-if (fetchButton && recipeResults) {
-  fetchButton.addEventListener("click", function() {
-    const proteinChoice = document.getElementById("Protein-choices").value;
-    const carbChoice = document.getElementById("Carbohydrates-choices").value;
-    const veg1Choice = document.getElementById("Vegetable1-choices").value;
-    const veg2Choice = document.getElementById("Vegetable2-choices").value;
-
-    const query = [proteinChoice, carbChoice, veg1Choice, veg2Choice]
-    .filter(Boolean)
-    .join(", ");
-
-  fetchAndDisplayRecipes(query, recipeResults); 
    });
-  }
-}); 
+  } 
+});
 
-function fetchAndDisplayRecipes(query, displayElement) {
-  const url = `https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=${query}`;
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "afe98ac037msh3ce3820dfc89dc5p153212jsn68ab7789434e",
-      "X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com",
-    },
-  };
-
-  fetch(url, options)
-  .then(response => response.json())
-  .then(data => {
-    displayRecipes(data, displayElement);
-  })
-  .catch(error => {
-    console.error("Error fetching recipes:", error);
-    displayElement.innerHTML = `<p>Error: ${error.message}</p>`;
-  });
-} 
-
-function displayRecipes(recipes, container) {
-  container.innerHTML = "";
-  if (recipes.length === 0) {
-    container.innerHTML = "<p>No recipes found based on your selections.</p>";
-    return;
-  }
-  recipes.forEach((recipe) => {
-    const cardDiv = document.createElement("div");
-    const cardBodyDiv = document.createElement("div");
-    const titleH5 = document.createElement("h5");
-    const ingredientsP = document.createElement("p");
-    const instructionsP = document.createElement("p");
-    const servingsP = document.createElement("p");
-  
-    cardDiv.className = "card text-center result-card";
-    cardBodyDiv.className = "card-body result-card-body";
-    titleH5.className = "card-title result-heading";
-    ingredientsP.className = "card-text result-ingredients";
-    instructionsP.className = "card-text result-instructions";
-    servingsP.className = "card-text result-servings";
-  
-    titleH5.textContent = recipe.title;
-    ingredientsP.textContent = `Ingredients: ${recipe.ingredients.replace(/\|/g, ', ')}`;
-    instructionsP.textContent = `Instructions: ${recipe.instructions}`;
-    servingsP.textContent = `Servings: ${recipe.servings}`;
-  
-    cardBodyDiv.appendChild(titleH5);
-    cardBodyDiv.appendChild(ingredientsP);
-    cardBodyDiv.appendChild(instructionsP);
-    cardBodyDiv.appendChild(servingsP);
-    cardDiv.appendChild(cardBodyDiv);
-    container.appendChild(cardDiv);
-  });
-
-  function displayItems(idName, items) {
-    const listElement = document.querySelector(idName);
-    items.forEach(item => {
-      const option = document.createElement("option");
-      option.textContent = item;
-      listElement.appendChild(option); 
-    })
-  }
-}
 function populateDropdown(selectElementId, items) {
   var selectElement = document.getElementById(selectElementId);
   items.forEach(function(item) {
--- assets/js/results.js
+++ assets/js/results.js
@@ -1,24 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const userChoices = JSON.parse(localStorage.getItem("userChoices") || '{}');
+  const userChoices = JSON.parse(localStorage.getItem("userChoices") || "{}");
 
-    if (!userChoices || Object.keys(userChoices).length === 0) {
-        console.error("no user choices found.");
-        return;
-    }
+  if (!userChoices || Object.keys(userChoices).length === 0) {
+    console.error("No user choices found.");
+    return;
+  }
 
-    const query = [userChoices.proteins, userChoices.carbohydrates, userChoices.vegetables1, userChoices.vegetables2]
-            .filter(Boolean)
-            .join("&");
+  const query = [
+    userChoices.proteins,
+    userChoices.carbohydrates,
+    userChoices.vegetables1,
+    userChoices.vegetables2,
+  ]
+    .filter(Boolean)
+    .join("&");
 
-    const recipeResultsContainer = document.getElementById("recipe-container");
+  const recipeResultsContainer = document.getElementById("recipe-container");
 
-    if(recipeResultsContainer) {
-        fetchRecipes(query, recipeResultsContainer);
-    } else {
-        console.error("Recipe results container not found."); 
-    }
-}); 
-    
+  if (recipeResultsContainer) {
+    fetchRecipes(query, recipeResultsContainer);
+  } else {
+    console.error("Recipe results container not found.");
+  }
+});
+
 function fetchRecipes(query, displayElement) {
   const url = `https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=${query}`;
   const options = {
@@ -29,59 +34,53 @@ function fetchRecipes(query, displayElement) {
     },
   };
 
-  console.log("making APi call with url:", url); 
-
   fetch(url, options)
-    .then((response) => {
-        console.log("APi response:", response); 
-      if (!response.ok) {
-        throw new Error(`API Error: ${response.status} ${response.statusText}`);
-      }
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
-        console.log("Fetched data: ", data); 
-        displayRecipes(data, displayElement);
+      displayRecipes(data, displayElement);
     })
     .catch((error) => {
-      console.error("There was a problem: ", error);
+      console.error("Error fetching recipes: ", error);
       displayElement.innerHTML = `<p>Error fetching recipes: ${error.message}</p>`;
     });
-}
 
-function displayRecipes(recipes, container) {
-  container.innerHTML = "";
+  function displayRecipes(recipes, container) {
+    container.innerHTML = "";
+    if (recipes.length === 0) {
+      container.innerHTML =
+        "<p> No recipes found based on your selections.</p>";
+      return;
+    }
 
-  if (recipes.length === 0) {
-    container.innerHTML = "<p> No recipes found based on your selections.</p>";
-    return;
+    recipes.forEach((recipe) => {
+      const cardDiv = document.createElement("div");
+      const cardBodyDiv = document.createElement("div");
+      const titleH5 = document.createElement("h5");
+      const ingredientsP = document.createElement("p");
+      const instructionsP = document.createElement("p");
+      const servingsP = document.createElement("p");
+
+      cardDiv.className = "card text-center result-card";
+      cardBodyDiv.className = "card-body result-card-body";
+      titleH5.className = "card-title result-heading";
+      ingredientsP.className = "card-text result-ingredients";
+      instructionsP.className = "card-text result-instructions";
+      servingsP.className = "card-text result-servings";
+
+      titleH5.textContent = recipe.title;
+      ingredientsP.textContent = `Ingredients: ${recipe.ingredients.replace(
+        /\|/g,
+        ", "
+      )}`;
+      instructionsP.textContent = `Instructions: ${recipe.instructions}`;
+      servingsP.textContent = `Servings: ${recipe.servings}`;
+
+      cardBodyDiv.appendChild(titleH5);
+      cardBodyDiv.appendChild(ingredientsP);
+      cardBodyDiv.appendChild(instructionsP);
+      cardBodyDiv.appendChild(servingsP);
+      cardDiv.appendChild(cardBodyDiv);
+      container.appendChild(cardDiv);
+    });
   }
-
-recipes.forEach((recipe) => {
-  const cardDiv = document.createElement("div");
-  const cardBodyDiv = document.createElement("div");
-  const titleH5 = document.createElement("h5");
-  const ingredientsP = document.createElement("p");
-  const instructionsP = document.createElement("p");
-  const servingsP = document.createElement("p");
-
-  cardDiv.className = "card text-center result-card";
-  cardBodyDiv.className = "card-body result-card-body";
-  titleH5.className = "card-title result-heading";
-  ingredientsP.className = "card-text result-ingredients";
-  instructionsP.className = "card-text result-instructions";
-  servingsP.className = "card-text result-servings";
-
-  titleH5.textContent = recipe.title;
-  ingredientsP.textContent = `Ingredients: ${recipe.ingredients.replace(/\|/g, ', ')}`;
-  instructionsP.textContent = `Instructions: ${recipe.instructions}`;
-  servingsP.textContent = `Servings: ${recipe.servings}`;
-
-  cardBodyDiv.appendChild(titleH5);
-  cardBodyDiv.appendChild(ingredientsP);
-  cardBodyDiv.appendChild(instructionsP);
-  cardBodyDiv.appendChild(servingsP);
-  cardDiv.appendChild(cardBodyDiv);
-  container.appendChild(cardDiv);
-});
 }
