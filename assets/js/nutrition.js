99d249d741e807dca112c02cf574dc2f9e923348
celiahouston <celia.houston99@gmail.com>
2024-01-01 14:35:36 +0000

    created new js file to test out nutrition api functionality and movedd js files into js folder. added previous code from history regarding nutrition api.

---
+++ assets/js/nutrition.js
@@ -0,0 +1,68 @@
+
+
+
+
+// previous code written to deal with Nutrition API
+
+// const nutritionButton = document.querySelector('.js-fetch-nutrition');
+// const nutritionResults = document.querySelector('.js-nutrition-section');
+// if (nutritionButton && nutritionResults) {
+
+//     nutritionButton.addEventListener('click', function () {
+//         const url = 'https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=1lb%20brisket%20with%20fries';
+//         const options = {
+//             method: 'GET',
+//             headers: {
+//                 'X-RapidAPI-Key': '99088a17fdmsh5b87115e6ccddc4p1da664jsnac40653791f8',
+//                 'X-RapidAPI-Host': 'nutrition-by-api-ninjas.p.rapidapi.com'
+//             }
+//         }
+
+//         fetch(url, options)
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error('Response was not ok');
+//                 }
+//                 return response.json();
+//             })
+//             .then(data => {
+//                 console.log(data)
+//                 let nutritionHTML = '';
+//                 for (let index = 0; index < data.length; index++) {
+//                     const element = data[index];
+//                     nutritionHTML += `<p>Name: ${element.name}</p>
+//           <div>Calories: ${element.calories}</div>
+//           <div>Carbohydrates (total) ${element.carbohydrates_total_g}</div>
+//           <div>Cholesterol ${element.cholesterol_mg}</div>
+//           <div>Saturarted Fat: ${element.fat_saturated_g}</div>
+//           <div>Fat (total): ${element.fat_total_g}</div>
+//           <div>Potassium: ${element.potassium_mg}</div>
+//           <div>Protein: ${element.protein_g}</div>
+//           <div>Sodium: ${element.sodium_mg}</div>
+//           <div>Sugar: ${element.sugar_g}</div>
+//           `;
+//                 }
+
+//                 nutritionResults.innerHTML = nutritionHTML;
+//             })
+//             .catch(error => {
+//                 console.error('There was a problem:', error);
+//             });
+//     });
+// } else {
+//     console.error('Nutrition or nutrition section was not found');
+// }
+
+// displayItems("#Protein-choices", proteins);
+// displayItems("#Carbohydrates-choices", carbohydrates);
+// displayItems("#Vegetable-choices", vegetables);
+// displayItems("#Vegetable-choices", vegetables);
+
+// const customerInput = "";
+// document.querySelectorAll("option").forEach((option) => {
+//     option.addEventListener("click", () => {
+//         console.log(option.innerHTML);
+//         customerInput += option.innerHTML + ' ';
+//     })
+// }); 
+// });
