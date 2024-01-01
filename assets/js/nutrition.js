eac1977c227a66840d864b54d56dff2d19004830
celiahouston <celia.houston99@gmail.com>
2024-01-01 16:10:56 +0000

    nutrition API working and implemented dynamically, code now within the results.js file

--- assets/js/nutrition.js
+++ assets/js/nutrition.js
@@ -1,83 +1,47 @@
-document.addEventListener("DOMContentLoaded", function () {
   function fetchAndDisplayNutrition(ingredients) {
-    const url =
-      "https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=1lb%20brisket%20with%20fries";
+    const url = `https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=${query}`;
     const options = {
       method: "GET",
       headers: {
         "X-RapidAPI-Key": "99088a17fdmsh5b87115e6ccddc4p1da664jsnac40653791f8",
         "X-RapidAPI-Host": "nutrition-by-api-ninjas.p.rapidapi.com",
-      }
+      },
     };
-  }
-});
 
     fetch(url, options)
-            .then(response => response.json()) 
-            .then(data => {
-                displayNutritionData(data); 
-            })
-            .catch(error => {
-                console.error("Error fetching nutrition data:", error); 
-            }); 
-    
+        .then(response => {
+            if (!response.ok) {
+                throw new Error ("Response not ok.");
+            }
+            return response.json(); 
+        })
+        .then(data => {
+            displayNutritionData(data);
+        })
+        .catch(error => {
+            console.error("Error fetching nutrition data:", error);
+        });
+    } 
+
+
 function displayNutritionData(data) {
+  const nutritionContainer = document.getElementById("nutrition-container");
+    nutritionContainer.nutritionHTML = "";
 
-}
-
-// on click of button function? 
-
-// previous code written to deal with Nutrition API
-
-// const nutritionButton = document.querySelector('.js-fetch-nutrition');
-// const nutritionResults = document.querySelector('.js-nutrition-section');
-// if (nutritionButton && nutritionResults) {
-
-//     nutritionButton.addEventListener('click', function () {
-
-//                 if (!response.ok) {
-//                     throw new Error('Response was not ok');
-//                 }
-//                 return response.json();
-//             })
-//             .then(data => {
-//                 console.log(data)
-//                 let nutritionHTML = '';
-//                 for (let index = 0; index < data.length; index++) {
-//                     const element = data[index];
-//                     nutritionHTML += `<p>Name: ${element.name}</p>
-//           <div>Calories: ${element.calories}</div>
-//           <div>Carbohydrates (total) ${element.carbohydrates_total_g}</div>
-//           <div>Cholesterol ${element.cholesterol_mg}</div>
-//           <div>Saturarted Fat: ${element.fat_saturated_g}</div>
-//           <div>Fat (total): ${element.fat_total_g}</div>
-//           <div>Potassium: ${element.potassium_mg}</div>
-//           <div>Protein: ${element.protein_g}</div>
-//           <div>Sodium: ${element.sodium_mg}</div>
-//           <div>Sugar: ${element.sugar_g}</div>
-//           `;
-//                 }
-
-//                 nutritionResults.innerHTML = nutritionHTML;
-//             })
-//             .catch(error => {
-//                 console.error('There was a problem:', error);
-//             });
-//     });
-// } else {
-//     console.error('Nutrition or nutrition section was not found');
-// }
-
-// displayItems("#Protein-choices", proteins);
-// displayItems("#Carbohydrates-choices", carbohydrates);
-// displayItems("#Vegetable-choices", vegetables);
-// displayItems("#Vegetable-choices", vegetables);
-
-// const customerInput = "";
-// document.querySelectorAll("option").forEach((option) => {
-//     option.addEventListener("click", () => {
-//         console.log(option.innerHTML);
-//         customerInput += option.innerHTML + ' ';
-//     })
-// });
-// });
+data.forEach(nutrition => {
+    let content = ` <div>
+        <p>Name: ${element.name}</p>
+        <p>Calories: ${element.calories}</p>
+        <p>Carbohydrates (total) ${element.carbohydrates_total_g}</p>
+        <p>Cholesterol ${element.cholesterol_mg}</p>
+        <p>Saturarted Fat: ${element.fat_saturated_g}</p>
+        <p>Fat (total): ${element.fat_total_g}</p>
+        <p>Potassium: ${element.potassium_mg}</p>
+        <p>Protein: ${element.protein_g}</p>
+        <p>Sodium: ${element.sodium_mg}</p>
+        <p>Sugar: ${element.sugar_g}</p>
+        </div> 
+        `;
+    nutritionContainer.innerHTML += content; 
+    }); 
+} 
